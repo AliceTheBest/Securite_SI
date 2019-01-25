b69aaf5fc02fd8f72945984b68c00058cbbd0000
melanie <mel.ep.morin@gmail.com>
2019-01-25 14:08:05 +0100

    form new password

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -2,23 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManager;
 use App\Form\Type\ChangePasswordType;
 
 /** @Route("/member") */
-class MemberController extends Controller {
+class MemberController extends AbstractController {
 
     /**
      * @Route("/")
      */
-    public function index() {
+    public function index (Request $request) {
 
-        $form = $this->get('form.factory')
-            ->createNamedBuilder(null)
+        $form = $this->createFormBuilder()
             ->add('_current_password', \Symfony\Component\Form\Extension\Core\Type\PasswordType::class, ['label' => 'Mot de passe actuel'])
             ->add('_new_password', \Symfony\Component\Form\Extension\Core\Type\PasswordType::class, ['label' => 'Nouveau mot de passe'])
             ->add('_new_password_confirm', \Symfony\Component\Form\Extension\Core\Type\PasswordType::class, ['label' => 'Confirmer le nouveau mot de passe'])
@@ -26,7 +26,17 @@ class MemberController extends Controller {
             ->add('ok', \Symfony\Component\Form\Extension\Core\Type\SubmitType::class, ['label' => 'Modifier', 'attr' => ['class' => 'btn-primary btn-block']])
             ->getForm();
 
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                dump($form->getData());
+                die();
+                $newpass = $form['_new_password']->getData();
+    
+                $this->addFlash('success', 'nouveau mot de passe '+$newpass);
+            }
+
         return $this->render('member/index.html.twig', ['mainNavMember'=>true, 'form' => $form->createView()]);
     }
+    
 
 }
